ac0e66f0bd5f5e30919d21d6988e72484c2f293a
KingOfTheGamesYT <53309562+KingOfTheGamesYT@users.noreply.github.com>
2025-01-06 17:12:44 -0500

    the workspace
    the start

---
+++ src/main/java/com/devmaster/devs_test/misc/Devs_Test.java
@@ -0,0 +1,40 @@
+package com.devmaster.devs_test.misc;
+
+import com.devmaster.devs_test.util.RegistryHandler;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.tags.ITag;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+@Mod("simplisticsuperswords")
+public class Devs_Test {
+    public static final Logger LOGGER = LogManager.getLogger("Dev's Test");
+    public static final String MOD_ID = "devs_test";
+    public static final ITag.INamedTag<Block> MINERS_DREAM_MINEABLE = BlockTags.makeWrapperTag(Devs_Test.MOD_ID+":miners_dream_breakable");
+    public static final ItemGroup TAB = new DangerZoneItems();
+
+    public Devs_Test() {
+       FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+        final IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+        modEventBus.addListener(this::setup);
+        RegistryHandler.init();
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+    }
+    private void doClientStuff(final FMLClientSetupEvent event) {
+    }
+}
+
---
+++ src/main/java/com/devmaster/devs_test/util/RegistryHandler.java
@@ -0,0 +1,34 @@
+package com.devmaster.devs_test.util;
+
+import com.devmaster.devs_test.misc.Devs_Test;
+
+import net.minecraft.block.Block;
+import net.minecraft.item.*;
+
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
+
+public class RegistryHandler {
+    public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, Devs_Test.MOD_ID);
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, Devs_Test.MOD_ID);
+
+    public static void init() {
+        BLOCKS.register(FMLJavaModLoadingContext.get().getModEventBus());
+        ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
+    }
+
+    //Armor
+
+    //Blocks
+
+    //Block Items
+
+    //Entities
+
+    //Items
+
+    //Tools and Weapons
+
+}
