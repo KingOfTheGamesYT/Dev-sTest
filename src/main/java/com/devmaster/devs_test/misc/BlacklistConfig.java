a6ffd93b9fc3e0f5ee43c2d88061d841e3b194dd
KingOfTheGamesYT <53309562+KingOfTheGamesYT@users.noreply.github.com>
2025-08-07 13:40:37 -0400

    randomized spawners
    in 1.20.1

---
+++ src/main/java/com/devmaster/devs_test/misc/BlacklistConfig.java
@@ -0,0 +1,36 @@
+package com.devmaster.devs_test.misc;
+
+import net.minecraft.resources.ResourceLocation;
+import net.minecraftforge.common.ForgeConfigSpec;
+import net.minecraftforge.registries.ForgeRegistries;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class BlacklistConfig {
+    public static ForgeConfigSpec COMMON_CONFIG;
+    public static ForgeConfigSpec.ConfigValue<List<? extends String>> ENTITY_BLACKLIST;
+
+    static {
+        ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+
+        builder.push("DungeonSpawnerBlacklist");
+
+        ENTITY_BLACKLIST = builder.defineList(
+            "blacklistedEntities",
+            Arrays.asList(
+                "minecraft:wither",
+                "minecraft:ender_dragon",
+                "minecraft:elder_guardian"
+            ),
+            obj -> {
+                if (!(obj instanceof String)) return false;
+                ResourceLocation id = ResourceLocation.tryParse((String) obj);
+                return id != null && ForgeRegistries.ENTITY_TYPES.containsKey(id);
+            }
+        );
+
+        builder.pop();
+        COMMON_CONFIG = builder.build();
+    }
+}
--- src/main/java/com/devmaster/devs_test/misc/Devs_Test.java
+++ src/main/java/com/devmaster/devs_test/misc/Devs_Test.java
@@ -2,21 +2,29 @@ package com.devmaster.devs_test.misc;
 
 import com.devmaster.devs_test.util.RegistryHandler;
 
-
+import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.block.Block;
 
+import net.minecraftforge.common.DungeonHooks;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
+import net.minecraftforge.registries.ForgeRegistries;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Mod("devs_test")
 public class Devs_Test {
@@ -31,10 +39,39 @@ public class Devs_Test {
         modEventBus.addListener(this::setup);
         RegistryHandler.init();
         MinecraftForge.EVENT_BUS.register(this);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, BlacklistConfig.COMMON_CONFIG);
+
     }
 
-    private void setup(final FMLCommonSetupEvent event) {
+    private void setup(FMLCommonSetupEvent event) {
+        DungeonHooks.removeDungeonMob(EntityType.ZOMBIE);
+        DungeonHooks.removeDungeonMob(EntityType.SKELETON);
+        DungeonHooks.removeDungeonMob(EntityType.SPIDER);
+
+        List<String> blacklist = (List<String>) BlacklistConfig.ENTITY_BLACKLIST.get();
+
+        List<EntityType<?>> validMobs = ForgeRegistries.ENTITY_TYPES.getValues().stream()
+                .filter(type -> {
+                    if (type == null || type.getCategory() == null || BuiltInRegistries.ENTITY_TYPE.getKey(type) == null) return false;
+                    if (type.getCategory().isFriendly()) return false;
+
+                    String id = BuiltInRegistries.ENTITY_TYPE.getKey(type).toString();
+                    if (blacklist.contains(id)) {
+                        System.out.println("[DungeonSpawner] Skipping blacklisted mob: " + id);
+                        return false;
+                    }
+
+                    return true;
+                })
+                .collect(Collectors.toList());
+
+        System.out.println("[RandomDungeonSpawners] Adding " + validMobs.size() + " mobs to dungeon list");
+
+        for (EntityType<?> type : validMobs) {
+            DungeonHooks.addDungeonMob(type, 100); // Equal weight
+        }
     }
+
     private void doClientStuff(final FMLClientSetupEvent event) {
     }
 }
